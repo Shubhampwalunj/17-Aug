e1f5bbc3424519e831dfd05c18119245ee59c3a6
Shubhampwalunj <86958175+Shubhampwalunj@users.noreply.github.com>
2022-08-26 11:05:16 +0530

    Add files via upload

---
+++ reverse the number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int no,rev=0,r,n;
+    cout<<"Enter any positive int number = ";
+    cin>>n;
+    no=n;
+    
+    while(no>0)
+    {
+        r=no%10;
+        cout<<"r=no%10 == "<<no<<"  "<<r<<'\n';
+        rev=rev*10+r;
+        cout<<"rev=rev*10+r == "<< rev<<'\n';
+        no=no/10;
+        cout<<"no=no/10 == "<<no<<'\n';
+    }
+    cout<<"\nReverse of the number"<<n<<"is ="<<rev<<"\n";
+    return 0;
+}
+
+
